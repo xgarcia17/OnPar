6c9d168958bfd31aff21c0d32eadc54f6ac6ca6d
Xavier Garcia <xmangarcia17@gmail.com>
2026-05-03 20:44:31 -0700

    feat: create simple landing page with color thems

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,16 +1,20 @@
-import { getHealth } from "@/lib/api";
-
 export default async function Home() {
-  const data = await getHealth();
-
   return (
-    <div className="padding-responsive min-h-screen flex flex-col items-center" style={{ paddingTop: '20vh' }}>
-      <h1 className="text-center h1-responsive font-bold text-danger padding-responsive">
+    <div
+      className="padding-responsive min-h-screen flex flex-col items-center"
+      style={{ paddingTop: "20vh" }}
+    >
+      <h1 className="text-center h1-responsive font-bold text-title m-12 title-shadow">
         OnPar
       </h1>
-      <p className="text-center p-responsive">
-        <em>{JSON.stringify(data)}</em>
-      </p>
+      <div className="mx-auto w-[80%] border-[5px] rounded-lg bg-text border-secondary min-h-[30vh] flex items-center">
+        <p className="text-center text-primary text-lg md:text-xl p-4">
+          Welcome to <em>OnPar</em>! <em>OnPar</em> is an application for you
+          and your friends to gameify your goal-tracking in a golf-inspired way.
+          Customize your goals and defined what it means for you to be "on
+          track," or as we say, "on par!"
+        </p>
+      </div>
     </div>
   );
 }
--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -4,16 +4,6 @@ export default {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {
-    extend: {
-      colors: {
-        primary: "var(--text-primary)",
-        secondary: "var(--text-secondary)",
-        muted: "var(--text-muted)",
-        brand: "var(--text-brand)",
-        danger: "var(--text-danger)",
-      },
-    },
-  },
+  theme: {},
   plugins: [],
 };
