5968f6665d8534049c00ee02d4c39f8eff77e9d9
Xavier Garcia <xmangarcia17@gmail.com>
2026-05-03 20:51:47 -0700

    fix: replace double-quote character with &quot; for linting

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -7,12 +7,12 @@ export default async function Home() {
       <h1 className="text-center h1-responsive font-bold text-title m-12 title-shadow">
         OnPar
       </h1>
-      <div className="mx-auto w-[80%] border-[5px] rounded-lg bg-text border-secondary min-h-[30vh] flex items-center">
+      <div className="mx-auto w-[80%] border-[5px] rounded- bg-text border-secondary min-h-[30vh] flex items-center">
         <p className="text-center text-primary text-lg md:text-xl p-4">
           Welcome to <em>OnPar</em>! <em>OnPar</em> is an application for you
           and your friends to gameify your goal-tracking in a golf-inspired way.
-          Customize your goals and defined what it means for you to be "on
-          track," or as we say, "on par!"
+          Customize your goals and defined what it means for you to be &quot;on
+          track,&quot; or as we say, &quot;on par!&quot;
         </p>
       </div>
     </div>
