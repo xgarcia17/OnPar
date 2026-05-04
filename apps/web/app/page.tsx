d2216d7b51b7069bbcc434ca4a6e5a24cff92df6
Xavier Garcia <xmangarcia17@gmail.com>
2026-05-03 21:00:39 -0700

    fix: prettier fighting a space

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -9,10 +9,10 @@ export default async function Home() {
       </h1>
       <div className="mx-auto w-[80%] border-[5px] rounded- bg-text border-secondary min-h-[30vh] flex items-center">
         <p className="text-center text-primary text-lg md:text-xl p-4">
-          Welcome to <em>OnPar</em>! <em>OnPar</em> is an application for you
-          and your friends to gameify your goal-tracking in a golf-inspired way.
-          Customize your goals and defined what it means for you to be &quot;on
-          track,&quot; or as we say, &quot;on par!&quot;
+          Welcome to <em>OnPar</em>! <em>OnPar</em>&nbsp;is an application for
+          you and your friends to gameify your goal-tracking in a golf-inspired
+          way. Customize your goals and defined what it means for you to be
+          &quot;on track,&quot; or as we say, &quot;on par!&quot;
         </p>
       </div>
     </div>
