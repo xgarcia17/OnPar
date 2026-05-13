246c737ff1fd148d2bd7b9868c99e6416d0055b6
Xavier Garcia <xmangarcia17@gmail.com>
2026-05-13 14:21:53 -0700

    call api in userSettingsContent

--- apps/web/lib/api/users.ts
+++ apps/web/lib/api/users.ts
@@ -2,10 +2,8 @@ import type { ApiResponse, User } from "@onpar/shared";
 
 const serverUserUrl: string = `${process.env.NEXT_PUBLIC_API_URL}/api/users`;
 
-export async function getUserById(userId: number) {
-  const res = await fetch(`${serverUserUrl}/${userId}`, {
-    next: { revalidate: 300 },
-  });
+export async function getUserById(userId: number): Promise<User> {
+  const res = await fetch(`${serverUserUrl}/${userId}`);
 
   if (!res.ok) {
     throw new Error(`HTTP error: ${res.status}`);
